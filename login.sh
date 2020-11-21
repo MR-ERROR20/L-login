6b332e5b00d9a49c7646cc295ef97f883f8cce97
LBOT GANSS <73551794+MR-ERROR20@users.noreply.github.com>
2020-11-21 19:25:58 +0700

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/usr/bin/bash
+clear
+echo -e "\e[1;35m
+  ,==. 
+/   66\ 
+\c     -_)
+  `)  ( 
+   /    \ 
+ /     \ \               
+( (    /\ \_   L - LOGIN
+ \ \  \ `--` 8=========D💦    
+ / /  /        
+(_(___)
+\e[1;35m[] Author : L[] Github : https://www.github.com/MR-ERROR20\e[0m"read -p $'\e[31mBuat Username Untuk Login :\e[0m ' usernameread -p $'\e[32mBuat Password Untuk Login :\e[0m ' passwordcd cd ..cd usr/etcrm motdrm bash.bashrccat <<LOGIN>bash.bashrctrap '' 2echo -e "\e[1;32m 
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+\e[0m"read -p $'\e[32m Username :\e[0m ' userread -s -p $'\e[32m Password :\e[0m ' passif [[ \$pass == $password && \$user == $username ]]; thenPS1='\033[1;32m\a┏\a \033[1;32m\a┏\a━\a┃ MASUKAN COMMAND SEPERTI BIASA ┃--┃ \@ ┃---| \d ┃\a\a┃\a \a\033[1;32m┗━ $user ━┛\033[1;36m\a┗\a┳\a━\a\033[1;32m\w\a ┗ ==>'shopt -s autocdshopt -s cdspellshopt -s checkhashshopt -s checkwinsizeshopt -s compat31shopt -s compat32shopt -s compat40shopt -s compat41shopt -s no_empty_cmd_completionshopt -s histverifyshopt -s histappendshopt -s dirspellshopt -s direxpandshopt -s compat43shopt -s compat32shopt -s lithistelseecho ""echo -e "\e[1;31m Your Password Is IncorrectBye :)\e[0m"sleep 3exitfitrap 2LOGINecho -e "\e[1;32m EXIT TERMUX DAN LIHAT KEAJAIBANNYA:v\e[0m"
